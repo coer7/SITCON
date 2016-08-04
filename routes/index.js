6399498fbe693beddad38479ec74b5aa0f1d85dd
coer7 <wl00739514@gmail.com>
2016-08-04 21:45:15 +0800

    login final v 10

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get('/register', function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express login' });
+  res.render('login', { title: 'Express login' ,"signed":!!req.body.username});
 });
 
 router.get('/personal_page', function(req, res, next) {
@@ -143,7 +143,7 @@ router.post('/login', function(req, res) {
 			}
 			if(bool){
 				console.log("Your username does not exist!");
-				res.render('index');
+				res.render('login',{"signed":false});
 			}
 		});
 		
@@ -151,48 +151,6 @@ router.post('/login', function(req, res) {
 	
 
 });
-/*
-//執行註冊
-router.post =('/register', function(req, res){
-	console.log('testttt');
-	if(req.body.password != req.body.password2){
-		console.log('密碼輸入不一致。');
-		console.log('第一次輸入的密碼：' + req.body.password);
-		console.log('第二次輸入的密碼：' + req.body.password2);
-		return res.redirect('/register');
-	}
-	else{
-		// Set our internal DB variable
-		var db = req.db;
-		// Get our form values. These rely on the "name" attributes
-		var userName = req.body.username;
-		console.log(userName);
-		var passWord = req.body.password;
-		console.log(passWord);
-		// Set our collection
-		var collection = db.get('usercollection');
-		// Submit to the DB
-		collection.insert({
-			"username" : userName,
-			"password" : passWord
-		}, function (err, doc) {
-			if (err) {
-				// If it failed, return error
-				res.send("There was a problem adding the information to the database.");
-			}
-			else {
-				// If it worked, set the header so the address bar doesn't still say /adduser
-				res.location("userlist");
-				// And forward to success page
-				res.redirect("userlist");
-			}
-		});
-		res.cookie('userid', req.body.username, { path: '/', signed: true});		
-		res.cookie('password', req.body.password, { path: '/', signed: true });
-		return res.redirect('/');
-	}
-});*/
-
 //檢查使用者登入狀態
 var isLogin = false;
 var checkLoginStatus = function(req, res){
